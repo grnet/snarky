bdf9f4227c6924a4c2b7393030a32df10296aada
fmerg <fmerg@grnet.gr>
2021-04-08 10:22:51 +0300

    Define and use PolyError

--- src/error.rs
+++ src/error.rs
@@ -30,3 +30,33 @@ impl fmt::Display for QAPError {
         )
     }
 }
+
+#[derive(Debug, Clone)]
+pub struct PolyError {
+    pub message: String,
+    pub file: String,
+    pub line: u32,
+    pub code: u32,
+}
+
+impl PolyError {
+    pub fn create(message: &str, file: &str, line: u32, code: u32) -> Self {
+        Self { 
+            message: message.to_string(), 
+            file: file.to_string(), 
+            line,
+            code,
+        }
+    }
+}
+
+impl fmt::Display for PolyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "PolyError[{}] ({}:{}): {}", 
+            self.code,
+            self.file, 
+            self.line, 
+            self.message
+        )
+    }
+}
