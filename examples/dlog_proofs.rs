82a2d7df9ad25de4f556e3b2c1bd600afb330b72
fmerg <fmerg@grnet.gr>
2021-04-09 01:11:35 +0300

    (dlog): Implement hashG1 (first iteration)

--- examples/dlog_proofs.rs
+++ examples/dlog_proofs.rs
@@ -5,7 +5,7 @@ pub fn main() {
     let elem_1 = mult_1!(G1_gen!(), scalar!(100));
     let elem_2 = mult_2!(G2_gen!(), scalar!(100));
     let phi = (elem_1, elem_2);
-    let witness = scalar!(666);
+    let witness = scalar!(100);
     let proof = prove_dlog(phi, witness);
     let verified = verify_dlog(phi, proof);
     assert!(verified);
