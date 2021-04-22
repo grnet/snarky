5f83db6b2bcfd521bcba561afefb3f6169964782
fmerg <fmerg@grnet.gr>
2021-04-23 00:07:52 +0300

    Start adding verification failure tests

--- protocol/tests/flow.rs
+++ protocol/tests/flow.rs
@@ -1,6 +1,7 @@
 //! Contains integration tests involving `snarky` public functions.
 //! Common test setup taken from the `common` module.
 
+use backend::*;
 use circuits::QAP;
 use protocol::{SRS, Trapdoor, BatchProof, Phase, Verification};
 use protocol;
@@ -43,4 +44,36 @@ fn test_flow_with_unit_trapdoor() {
     protocol::update(&qap, &mut srs, &mut batch, Phase::TWO);
     let res = protocol::verify(&qap, &srs, &batch);
     assert!(res.as_bool());
+}
+
+#[test]
+fn test_flow_with_tampered_phase_1_proof() {
+    let (m, n, l) = (5, 4, 3);
+    let qap = QAP::create_default(m, n, l).unwrap();
+    let (mut srs, trp) = SRS::setup_with_unit_trapdoor(&qap);
+    assert_eq!(trp, Trapdoor::from_units());
+    let mut batch = BatchProof::initiate();
+    protocol::update(&qap, &mut srs, &mut batch, Phase::ONE);
+    protocol::update(&qap, &mut srs, &mut batch, Phase::TWO);
+    // tamper phase 1 proof
+    let val = &batch.batch_1[0][0].com.0;
+    batch.batch_1[0][0].com.0 = add1!(val, genG1!());
+    let res = protocol::verify(&qap, &srs, &batch);
+    assert!(!res.as_bool());
+}
+
+#[test]
+fn test_flow_with_tampered_phase_2_proof() {
+    let (m, n, l) = (5, 4, 3);
+    let qap = QAP::create_default(m, n, l).unwrap();
+    let (mut srs, trp) = SRS::setup_with_unit_trapdoor(&qap);
+    assert_eq!(trp, Trapdoor::from_units());
+    let mut batch = BatchProof::initiate();
+    protocol::update(&qap, &mut srs, &mut batch, Phase::ONE);
+    protocol::update(&qap, &mut srs, &mut batch, Phase::TWO);
+    // tamper phase 1 proof
+    let val = &batch.batch_2[0].com.0;
+    batch.batch_2[0].com.0 = add1!(val, genG1!());
+    let res = protocol::verify(&qap, &srs, &batch);
+    assert!(!res.as_bool());
 }
